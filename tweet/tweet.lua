533a4a2fb5a06c4a99d578ee4e39d26932602eeb
Amirgamer5659 <73234407+Amirgamer5659@users.noreply.github.com>
2020-10-29 14:04:50 +0330

    Add files via upload

---
+++ tweet/tweet.lua
@@ -0,0 +1,10 @@
+RegisterCommand('tweet', function(source, args, rawCommand)
+    local playerName = GetPlayerName(source)
+    local msg = rawCommand:sub(6)
+    local name = getIdentity(source)
+    fal = name.firstname .. " " .. name.lastname
+    TriggerClientEvent('chat:addMessage', -1, {
+        template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(28, 160, 242, 0.6); border-radius: 3px;"><i class="fab fa-twitter"></i> @{0}:<br> {1}</div>',
+        args = { fal, msg }
+    })
+end, false)
